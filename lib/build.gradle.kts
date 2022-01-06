0489a4615c8b7394f959841e49c5ec99fdeead14
Mohamed Metwalli <mohamed.metwalli@sap.com>
2022-01-06 22:45:38 +0100

    Spotless

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -1,6 +1,9 @@
+import com.diffplug.gradle.spotless.SpotlessExtension
+
 plugins {
     kotlin("jvm") version "1.6.10"
     `java-library`
+    id("com.diffplug.spotless").version("6.0.0")
 }
 
 group = "de.rki.jfn"
@@ -21,4 +24,12 @@ dependencies {
     // Testing
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.2")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.2")
+}
+
+configure<SpotlessExtension> {
+    kotlin {
+        target("**/*.kt")
+        targetExclude("$buildDir/**/*.kt", "**/*.gradle.kts")
+        ktlint("0.43.0")
+    }
 }
