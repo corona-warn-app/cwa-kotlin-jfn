670d7c8e10a9bfd9f926d2505743b9d003c7180d
Mohamed Metwalli <mohamed.metwalli@sap.com>
2022-01-13 13:00:08 +0100

    Reduce

--- src/test/kotlin/CommonTestCaseProvider.kt
+++ src/test/kotlin/CommonTestCaseProvider.kt
@@ -15,7 +15,7 @@ class CommonTestCaseProvider : ArgumentsProvider {
         val testCases = tree.get("testCases") as ArrayNode
         return testCases
             // for single test execution
-            // .filter { it.get("title").textValue().startsWith("test case 145") }
+            .filter { it.get("title").textValue().startsWith("reduce -") }
             .map { Arguments.of(Named.of(it.get("title").textValue(), it)) }.stream()
     }
 }
