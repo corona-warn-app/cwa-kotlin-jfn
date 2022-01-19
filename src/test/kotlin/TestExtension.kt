f71c923139cad0ebe4279c20319483d360c63fc5
Mohamed Metwalli <mohamed.metwalli@sap.com>
2022-01-19 16:20:15 +0100

    Fix test

--- src/test/kotlin/TestExtension.kt
+++ src/test/kotlin/TestExtension.kt
@@ -1,18 +1,20 @@
 import com.fasterxml.jackson.databind.JsonNode
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.databind.node.ValueNode
+import de.rki.jfn.JsonFunctionsEngine
 import de.rki.jfn.evaluateLogic
 
 val objectMapper = ObjectMapper()
+val engine = JsonFunctionsEngine()
 
 fun String.toJsonNode(): JsonNode = objectMapper.readTree(this)
 
-fun String.evaluateJson(data: String): JsonNode = evaluateLogic(
+fun String.evaluateJson(data: String): JsonNode = engine.evaluate(
     this.toJsonNode(),
     data.toJsonNode()
 )
 
-fun String.evaluateJson(data: ValueNode): JsonNode = evaluateLogic(
+fun String.evaluateJson(data: ValueNode): JsonNode = engine.evaluate(
     this.toJsonNode(),
     data
 )
