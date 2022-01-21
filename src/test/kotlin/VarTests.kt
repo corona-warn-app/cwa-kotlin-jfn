d8225b98b0bc3f6ee2379a4fb3f3d6b15e5e21c1
Lukas Lechner <office@lukaslechner.com>
2022-01-21 14:00:32 +0100

    Refactor existing VarTests

--- src/test/kotlin/VarTests.kt
+++ src/test/kotlin/VarTests.kt
@@ -1,9 +1,9 @@
+
 import com.fasterxml.jackson.databind.node.BooleanNode
 import com.fasterxml.jackson.databind.node.IntNode
-import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import de.rki.jfn.JsonFunctionsEngine
+import io.kotest.matchers.shouldBe
 import org.junit.jupiter.api.Test
-import kotlin.test.assertEquals
 
 class VarTests {
 
@@ -11,86 +11,63 @@ class VarTests {
 
     @Test
     fun `var should return data on empty array`() {
-        val logic = jacksonObjectMapper().readTree(
-            """
-            {
-                "var" : [ ]
-            }"""
-        )
-
-        val data = jacksonObjectMapper().readTree("1")
-        assertEquals(IntNode.valueOf(1), engine.evaluate(logic, data))
+        """
+        {
+            "var" : [ ]
+        }
+        """.evaluateJson("1") shouldBe IntNode.valueOf(1)
     }
 
     @Test
-    fun `var should return data on blank string`() {
-        val logic = jacksonObjectMapper().readTree(
-            """
-            {
-                "var" : ""
-            }"""
-        )
-
-        val data = jacksonObjectMapper().readTree("2")
-        assertEquals(IntNode.valueOf(2), engine.evaluate(logic, data))
+    fun `var should return data on empty string`() {
+        """
+        {
+            "var" : ""
+        }
+        """.evaluateJson("2") shouldBe IntNode.valueOf(2)
     }
 
     @Test
-    fun `var should last element of array if size of array is greater than 1`() {
-        val logic2elements = jacksonObjectMapper().readTree(
-            """
-            {
-                "var" : [1,2]
-            }"""
-        )
+    fun `var should return last element of array if size of array is greater than 1`() {
+        """
+        {
+            "var" : [1,2]
+        }
+        """.evaluateJson("{}") shouldBe IntNode.valueOf(2)
 
-        val data = jacksonObjectMapper().readTree("{}")
-        assertEquals(IntNode.valueOf(2), engine.evaluate(logic2elements, data))
-
-        val logic3elements = jacksonObjectMapper().readTree(
-            """
-            {
-                "var" : [1,2,3]
-            }"""
-        )
-        assertEquals(IntNode.valueOf(3), engine.evaluate(logic3elements, data))
+        """
+        {
+            "var" : [1,2,3]
+        }
+        """.evaluateJson("{}") shouldBe IntNode.valueOf(3)
     }
 
     @Test
     fun `var should return data on null`() {
-        val logic = jacksonObjectMapper().readTree(
-            """
-            {
-                "var" : null
-            }"""
-        )
-
-        val data = jacksonObjectMapper().readTree("3")
-        assertEquals(IntNode.valueOf(3), engine.evaluate(logic, data))
+        """
+        {
+            "var" : null
+        }
+        """.evaluateJson("1") shouldBe IntNode.valueOf(1)
     }
 
     @Test
     fun `var should be able to declare operations`() {
-        val logic = jacksonObjectMapper().readTree(
-            """
-            {
-                "var" : [ 
-                    {
-                        "<" : [ 
-                            {
-                                "var" : "temp"
-                            }, 
-                            100 
-                        ]
-                    }
-                ]
-            }"""
-        )
+        val logic = """
+        {
+            "var" : [ 
+                {
+                    "<" : [ 
+                        {
+                            "var" : "temp"
+                        }, 
+                        100 
+                    ]
+                }
+            ]
+        }"""
 
-        val dataTemp99 = jacksonObjectMapper().readTree("""{"temp" : 99}""")
-        assertEquals(BooleanNode.TRUE, engine.evaluate(logic, dataTemp99))
-
-        val dataTemp100 = jacksonObjectMapper().readTree("""{"temp" : 100}""")
-        assertEquals(BooleanNode.FALSE, engine.evaluate(logic, dataTemp100))
+        logic.evaluateJson("""{"temp" : 99}""") shouldBe BooleanNode.TRUE
+        logic.evaluateJson("""{"temp" : 100}""") shouldBe BooleanNode.FALSE
     }
 }
