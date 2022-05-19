ad1ad17528734a77e6f5a33136c1ae64a2d68a01
Mohamed Metwalli <mohamed.metwalli@sap.com>
2022-05-19 22:28:56 +0200

    Class loader

--- src/test/kotlin/CommonTestCaseProvider.kt
+++ src/test/kotlin/CommonTestCaseProvider.kt
@@ -4,16 +4,14 @@ import org.junit.jupiter.api.Named
 import org.junit.jupiter.api.extension.ExtensionContext
 import org.junit.jupiter.params.provider.Arguments
 import org.junit.jupiter.params.provider.ArgumentsProvider
-import java.io.File
 import java.util.stream.Stream
 
 class CommonTestCaseProvider : ArgumentsProvider {
 
     override fun provideArguments(context: ExtensionContext?): Stream<out Arguments> {
-        val testsFile = File("src/test/resources/jfn-common-test-cases.gen.json")
+        val testsFile = javaClass.classLoader.getResource("jfn-common-test-cases.gen.json")
         val tree = ObjectMapper().readTree(testsFile)
         val testCases = tree.get("testCases") as ArrayNode
-        return testCases
-            .map { Arguments.of(Named.of(it.get("title").textValue(), it)) }.stream()
+        return testCases.map { Arguments.of(Named.of(it.get("title").textValue(), it)) }.stream()
     }
 }
