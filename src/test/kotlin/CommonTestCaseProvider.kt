1de29c69b24b271a7c9e7944dd7a91d32a25dd4b
Maximilian Lenkeit <2060790+mlenkeit@users.noreply.github.com>
2022-09-14 14:11:39 +0200

    chore: support common functions in test cases (#43)
    * chore: support common functions in test cases
    
    if a file with shared test cases such as `jfn-common-test-cases.json`
    contains a top-level property `commonFunctions`, it is registered as
    functions to each test case in that file. This has the potentially to
    significantly reduce the file size of shared test cases.
    
    * refactor: inline commonFunctions in map operation

--- src/test/kotlin/CommonTestCaseProvider.kt
+++ src/test/kotlin/CommonTestCaseProvider.kt
@@ -1,5 +1,6 @@
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.databind.node.ArrayNode
+import com.fasterxml.jackson.databind.node.ObjectNode
 import org.junit.jupiter.api.Named
 import org.junit.jupiter.api.extension.ExtensionContext
 import org.junit.jupiter.params.provider.Arguments
@@ -12,6 +13,15 @@ class CommonTestCaseProvider : ArgumentsProvider {
         val testsFile = javaClass.classLoader.getResource("jfn-common-test-cases.gen.json")
         val tree = ObjectMapper().readTree(testsFile)
         val testCases = tree.get("testCases") as ArrayNode
-        return testCases.map { Arguments.of(Named.of(it.get("title").textValue(), it)) }.stream()
+        val commonFunctions = tree.get("commonFunctions")
+
+        return testCases.map { case ->
+            val newCase = if (commonFunctions != null) {
+                (case as ObjectNode).set<ArrayNode>("functions", commonFunctions)
+            } else {
+                case
+            }
+            Arguments.of(Named.of(newCase.get("title").textValue(), newCase))
+        }.stream()
     }
 }
